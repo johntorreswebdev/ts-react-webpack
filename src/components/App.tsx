dc7849970703843eab05a4fe8fc199181be21b31
Fabian Torres <john_torres@oliver.com.pe>
2021-06-10 01:42:30 -0500

    first commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ClickCounter from "./ClickCounter";
 
 const App = () => {
+  const name = "John Fabian";
+
   return (
     <div>
+      <h2>Hi my name is {name}</h2>
       <h1>
-        {" "}
-        Hello ts-react-webpack a good course - {process.env.NODE_ENV}{" "}
+        Hello ts-react-webpack a good course - {process.env.NODE_ENV}
         {process.env.name}
       </h1>
       <ClickCounter />
